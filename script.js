dc3b881772bc8a4b3751dff613311d0c32f0217f
fryylord <rob@mchays.com>
2022-09-28 22:50:20 -0500

    js generates console password

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var chars ={ 
-  upperAlpha: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-  lowerAlpha: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  digit: ["0","1","2","3","4","5","6","7","8","9"],
-  specChar: ["!","@","#","$","%","^","&","*","(",")"]}
-var passlength = 12;
-var items1 = chars.upperAlpha
-var items2 = chars.lowerAlpha
-var items3 = chars.digit
-var items4 = chars.specChar
 
-function random_item(items1)
-{return items1[Math.floor(Math.random()*items1.length)];}
+var chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+"r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$",
+"%","^","&","*","(",")"];
 
-function random_item(items2)
-{return items2[Math.floor(Math.random()*items2.length)];}
 
-function random_item(items3)
-{return items3[Math.floor(Math.random()*items3.length)];}
+var passLength = 10;
 
-function random_item(items4)
-{return items4[Math.floor(Math.random()*items4.length)];}
+var passcode = ""
+for (let i = 1; i <= passLength; i++) {
+  console.log(random_item(chars));
 
-console.log(random_item(items1),random_item(items2),random_item(items3),random_item(items4), 
-random_item(items1),random_item(items2),random_item(items3),random_item(items4),random_item(items1),
-random_item(items2),random_item(items3),random_item(items4));
+  function random_item(chars)
+  {return chars[Math.floor(Math.random()*chars.length)];}
+}
 
 //function writePassword()
 
 
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+//var password = generatePassword();
+//var passwordText = document.querySelector("#password");
 
 //passwordText.value = password;
 

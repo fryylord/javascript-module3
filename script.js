47db4df79d49823880bb42551d3408fe03322eaf
fryylord <rob@mchays.com>
2022-09-30 11:41:50 -0500

    adding filter

--- script.js
+++ script.js
@@ -6,40 +6,40 @@ var chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q"
 "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$",
 "%","^","&","*","(",")"];
 
-// var upperAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
-// "U","V","W","X","Y","Z"]
+var upperAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z"]
 
-// var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-// "r","s","t","u","v","w","x","y","z"]
+var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+"r","s","t","u","v","w","x","y","z"]
 
-// var num = ["0","1","2","3","4","5","6","7","8","9"]
+var num = ["0","1","2","3","4","5","6","7","8","9"]
 
-// var sym = ["!","@","#","$","%","^","&","*","(",")"]
+var sym = ["!","@","#","$","%","^","&","*","(",")"]
 
 var passLength = 10;
 
-var passcode = ""
+var pw = ""
 for (let i = 5; i <= passLength; i++) {
-  console.log(random_item(chars));
+//  console.log(random_item(chars));
 
-  function random_item(chars)
+  function random_item(chars) {
   {return chars[Math.floor(Math.random()*chars.length)];}
+}}
+
+var password = [password()]
+
+function password() {
+if (pw.includes(upperAlpha) && pw.includes(lowerAlpha) && pw.includes(num) && pw.includes(sym)) {
+    console.log(pw)
+  }
+  else {random_item(chars)};
 }
+// function writePassword()
+
 
 // var password = generatePassword();
-// if (passcode.includes(upperAlpha)) {
+// var passwordText = document.querySelector("#password");
 
+// passwordText.value = password;
 
-// }
-//   else {random_items(chars)}
-
-  
-//function writePassword()
-
-
-//var password = generatePassword();
-//var passwordText = document.querySelector("#password");
-
-//passwordText.value = password;
-
-//generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);

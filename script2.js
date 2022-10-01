2369f028ef937c497da1645f5cc8a67a1feaa5d9
fryylord <rob@mchays.com>
2022-09-30 22:17:12 -0500

    revamped password generator code

--- script2.js
+++ script2.js
@@ -1,32 +1,63 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
-"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-"r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$",
-"%","^","&","*","(",")"];
+const numbers = '1234567890'
+const upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCases = 'abcdefghijklmnopqrstuvwxyz'
+const symbols = '!?#$%&*+-='
 
-var upperAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
-"U","V","W","X","Y","Z"]
+const passLength = 12
 
-var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-"r","s","t","u","v","w","x","y","z"]
+let charPool = numbers + upperCases + lowerCases + symbols
 
-var num = ["0","1","2","3","4","5","6","7","8","9"]
+generateBtn.addEventListener("click", generatesPass);
 
-var sym = ["!","@","#","$","%","^","&","*","(",")"]
+function generatesPass() {
+  const randomPass = []
+  for (let i = 0; i < 1; i++) {
+    randomPass
+      .push(Array(passLength)
+        .fill(charPool)
+        .map(x => x[Math.floor(Math.random() * x.length)])
+        .join('')
+      )
+  }
+  console.log(randomPass)
+}
 
-var passLength = 10;
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-var pw = ""
-for (let i = 1; i <= passLength; i++) {
- console.log(randomItem(chars));
-  function randomItem(chars) {
-  {return chars[Math.floor(Math.random()*chars.length)];}
-}}
+// var chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+// "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+// "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$",
+// "%","^","&","*","(",")"];
 
-// var password = [passString()]
+// var upperAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+// "U","V","W","X","Y","Z"]
 
+// var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+// "r","s","t","u","v","w","x","y","z"]
+
+// var num = ["0","1","2","3","4","5","6","7","8","9"]
+
+// var sym = ["!","@","#","$","%","^","&","*","(",")"]
+
+// var passLength = 10;
+
+// var pw = ""
+// for (let i = 0; i <= passLength; i++) {
+// console.log (randomItem(chars))
+//   function randomItem(chars) {
+//   {return (chars[Math.floor(Math.random()*chars.length)]);}
+// }}
+
+// document.getElementById("pw").value = password;
+
+// console.log(password)
+
+// var password = [pw]
+
+// console.log(password)
 // function passString(pw) {
 // if (pw.includes(upperAlpha) && pw.includes(lowerAlpha) && pw.includes(num) && pw.includes(sym)) {
 //    return
